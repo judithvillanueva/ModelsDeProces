1d93da2dabdb993c45ec9039005e805918a12a79
Judith Villanueva moreno <judvillanueva@MacBook-Pro-de-Judith.local>
2019-03-06 20:32:37 +0100

    gairebé funciona la pàgina principal

--- PractLlibreria/urls.py
+++ PractLlibreria/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^Biblioteca/', include (('recursosBiblio.urls','recursosBiblio'), namespace = 'recBib')),
+    url(r'^login/$', LoginView.as_view(),name='login'),
+    url(r'^logout/$', LogoutView.as_view(),name='logout'),
 ]
--- recursosBiblio/models.py
+++ recursosBiblio/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Book (models.Model) :
-    ISBN = models.BigIntegerField(primary_Key=True)
+    ISBN = models.BigIntegerField(primary_key=True)
     titol = models.TextField()
     genere = models.TextField()
     edicio = models.TextField()
