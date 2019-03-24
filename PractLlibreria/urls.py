6e407244d86b2ffd1aa6a78d0b72380acd096503
mireiamoix <mma20@alumnes.udl.cat>
2019-03-24 12:07:17 +0100

    DB pujada

--- PractLlibreria/urls.py
+++ PractLlibreria/urls.py
@@ -17,14 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.contrib.auth.views import LoginView, LogoutView
-from django.views.generic import TemplateView
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
-    url(r'^Biblioteca/', include(('recursosBiblio.urls', 'recursosBiblio'), namespace='recBib')),
+    url(r'^', include(('recursosBiblio.urls', 'recursosBiblio'), namespace='recBib')),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
-    # url(r'^$', TemplateView.as_view(template_name="recursosBiblio/templates/recursosBiblio/llibres.html"), name="Allbooks"),
 
 ]
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ModelsDeProces
 Pràctica 2 Models de Proces UDL
+
+
+Projecte de:
+- Mireia Moix Atienza
+- Judith Villanueva Moreno
+
+Login:
+- User: mire
+- Password: mirejud
+
+
+
+
